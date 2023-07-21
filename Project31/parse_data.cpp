341490d9ef45f4547e07f9824794d4f84a88aa32
spaceOfSoul <ychh1123@gmail.com>
2023-07-21 14:17:17 +0900

    초기 커밋.

---
+++ Project31/parse_data.cpp
@@ -0,0 +1,298 @@
+#include "parse_data.h"
+
+#include <iostream>
+#include <unordered_map>
+#include <sstream>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <vector>
+
+std::string Trim(std::string& str)
+{
+	str.erase(std::remove_if(str.begin(), str.end(), ::isspace), str.end());
+	return str;
+}
+
+// note info
+void note_info(std::string str)
+{
+	// Trim leading/trailing white space
+	str = Trim(str);
+
+	// Split the string into a vector of strings
+	std::vector<std::string> tokens;
+	std::istringstream ss(str);
+	std::string buffer;
+	while (std::getline(ss, buffer, ','))
+	{
+		tokens.push_back(Trim(buffer));
+	}
+
+	// Convert each string in the vector to an integer
+	std::vector<int> row;
+	for (const auto& token : tokens)
+	{
+		row.push_back(std::stoi(token));
+	}
+
+	int key = 0;
+	switch (row[0])  // 0th value (key position)
+	{
+	case 64:
+		key = 0;
+		break;
+	case 192:
+		key = 1;
+		break;
+	case 320:
+		key = 2;
+		break;
+	case 448:
+		key = 3;
+		break;
+	}
+
+	NotePoints[row[2]][key] = 1;     // Add note
+	Last_Note_pos = row[2];          // Update last note position
+	if (row[3] == 128) {             // Check if it's a long note (0: single note, 128: long note)
+		for (int n = row[2]; n <= row[5]; n++) // Fill until the end of long note : up to row[5]
+			ImagePoints[n][key] = 2;  // Fill long note
+	}
+	else
+	{
+		ImagePoints[row[2]][key] = 1; // Fill single note
+	}
+}
+
+
+
+// ReadProperty
+// ------------------------------------------------------------------------------------------
+// General Section 읽기
+void ReadProperty_General(std::string str)
+{
+	std::istringstream ss(str);
+	std::string buffer;
+	std::vector<std::string> tokens;
+
+	while (std::getline(ss, buffer, ':'))
+	{
+		buffer = Trim(buffer); // Assuming Trim() function trims the string and returns it
+		tokens.push_back(buffer);
+	}
+
+	if (tokens.size() > 1)
+	{
+		if (tokens[0] == "AudioFilename")
+		{
+			M_General.AudioFilename = tokens[1];
+		}
+		else if (tokens[0] == "AudioLeadIn")
+		{
+			M_General.AudioLeadIn = std::stoi(tokens[1]);
+		}
+		else if (tokens[0] == "PreviewTime")
+		{
+			M_General.PreviewTime = std::stoi(tokens[1]);
+		}
+		else if (tokens[0] == "Countdown")
+		{
+			M_General.Countdown = std::stoi(tokens[1]);
+		}
+		else if (tokens[0] == "StackLeniency")
+		{
+			M_General.StackLeniency = std::stof(tokens[1]);
+		}
+	}
+}
+
+
+// MetaData Section 읽기
+void ReadProperty_MetaData(std::string str)
+{
+	std::istringstream ss(str);
+	std::string buffer;
+	std::vector<std::string> tokens;
+
+	while (std::getline(ss, buffer, ':'))
+	{
+		buffer = Trim(buffer);
+		tokens.push_back(buffer);
+	}
+
+	// Process tokens...
+	if (tokens[0] == "Title")
+	{
+		M_MetaData.Title = tokens[1];
+	}
+	else if (tokens[0] == "TitleUnicode")
+	{
+		M_MetaData.TitleUnicode = tokens[1];
+	}
+	else if (tokens[0] == "Artist")
+	{
+		M_MetaData.Artist = tokens[1];
+	}
+	else if (tokens[0] == "ArtistUnicode")
+	{
+		M_MetaData.ArtistUnicode = tokens[1];
+	}
+	else if (tokens[0] == "Creator")
+	{
+		M_MetaData.Creator = tokens[1];
+	}
+	else if (tokens[0] == "Version")
+	{
+		M_MetaData.Version = tokens[1];
+	}
+	else if (tokens[0] == "Source")
+	{
+		M_MetaData.Source = tokens[1];
+	}
+	else if (tokens[0] == "Tags")
+	{
+		M_MetaData.Tags = tokens[1];
+	}
+	else if (tokens[0] == "BeatmapID")
+	{
+		M_MetaData.BeatmapID = std::stoi(tokens[1]);
+	}
+	else if (tokens[0] == "BeatmapSetID")
+	{
+		M_MetaData.BeatmapSetID = std::stoi(tokens[1]);
+	}
+}
+
+
+// Difficulty Section 읽기
+void ReadProperty_Difficulty(std::string str)
+{
+	std::istringstream ss(str);
+	std::string buffer;
+	std::vector<std::string> tokens;
+
+	while (std::getline(ss, buffer, ':'))
+	{
+		buffer = Trim(buffer);
+		tokens.push_back(buffer);
+	}
+
+	// Process tokens...
+	if (tokens[0] == "HPDrainRate")
+	{
+		M_Difficulty.HPDrainRate = std::stof(tokens[1]);
+	}
+	else if (tokens[0] == "CircleSize")
+	{
+		M_Difficulty.CircleSize = std::stof(tokens[1]);
+	}
+	else if (tokens[0] == "OverallDifficulty")
+	{
+		M_Difficulty.OverallDifficulty = std::stof(tokens[1]);
+	}
+	else if (tokens[0] == "ApproachRate")
+	{
+		M_Difficulty.ApproachRate = std::stof(tokens[1]);
+	}
+}
+
+
+// TimingPoint Section 읽기
+void ReadProperty_TimingPoint(std::string str)
+{
+	std::istringstream ss(str);
+	std::string buffer;
+	std::vector<std::string> tokens;
+
+	while (std::getline(ss, buffer, ','))
+	{
+		buffer = Trim(buffer);
+		tokens.push_back(buffer);
+	}
+
+	// Process tokens...
+	if (tokens.size() >= 8)  // Ensure there are enough tokens
+	{
+		TimingPoints[TPoint_array_section].time = std::stoi(tokens[0]);
+		TimingPoints[TPoint_array_section].meter = std::stoi(tokens[2]);
+		TimingPoints[TPoint_array_section].Volume = std::stoi(tokens[5]);
+		TimingPoints[TPoint_array_section].uninherited = std::stoi(tokens[6]);
+		TimingPoints[TPoint_array_section].effects = std::stoi(tokens[7]);
+		TimingPoints[TPoint_array_section].beatLength = std::stof(tokens[1]);
+	}
+	else
+	{
+		// Handle error: not enough tokens
+	}
+
+	TPoint_array_section++;
+}
+
+
+
+// ------------------------------------------------------------------------------------------
+// 구역구별
+void ReadLine_Check(std::string str, int section) {
+	switch (section)
+	{
+	case S_GENERAL:
+		ReadProperty_General(str);     // Basic settings
+		break;
+	case S_METADATA:
+		ReadProperty_MetaData(str);    // Meta data (title, name, etc.)
+		break;
+	case S_DIFFICULTY:
+		ReadProperty_Difficulty(str);  // Difficulty settings
+		break;
+	case S_TIMINGPOINT:
+		ReadProperty_TimingPoint(str); // Timing settings
+		break;
+	case S_HOBJECT:
+		note_info(str);                // Note settings
+		break;
+	default:
+		// pass
+		// Not applicable.
+		// s_event, s_editor
+		break;
+	}
+}
+
+int LoadMapFile(std::string beatmap) // Load map file
+{
+	std::ifstream file(beatmap);
+	if (file.is_open())
+	{
+		std::unordered_map<std::string, int> sectionMap = {
+			{"[General]\n", S_GENERAL},
+			{"[Editor]\n", S_EDITOR},
+			{"[Metadata]\n", S_METADATA},
+			{"[Difficulty]\n", S_DIFFICULTY},
+			{"[Events]\n", S_EVENTS},
+			{"[TimingPoints]\n", S_TIMINGPOINT},
+			{"[HitObjects]\n", S_HOBJECT}
+		};
+
+		std::string strTemp;
+		int section = 0;
+
+		while (std::getline(file, strTemp))
+		{
+			auto sectionIt = sectionMap.find(strTemp);
+			if (sectionIt != sectionMap.end()) // Section modification
+			{
+				section = sectionIt->second;
+			}
+			else  // Information processing
+			{
+				ReadLine_Check(strTemp, section);
+			}
+		}
+		file.close();
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
---
+++ Project31/parse_data.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <string>
+#include <vector>
+
+#define S_GENERAL 1
+#define S_EDITOR 2
+#define S_METADATA 3
+#define S_DIFFICULTY 4
+#define S_EVENTS 5
+#define S_TIMINGPOINT 6
+#define S_HOBJECT 7
+
+#define MAX_TSTAMP 300000
+#define M_ROW 4
+#define MAX_PSTAMP 2000
+
+struct General_Set {
+    std::string AudioFilename;
+    int AudioLeadIn;
+    int PreviewTime;
+    int Countdown;
+    float StackLeniency;
+};
+
+struct MetaData_Set {
+    std::string Title;
+    std::string TitleUnicode;
+    std::string Artist;
+    std::string ArtistUnicode;
+    std::string Creator;
+    std::string Version;
+    std::string Source;
+    std::string Tags;
+    int BeatmapID;
+    int BeatmapSetID;
+};
+
+struct Difficulty_Set {
+    float HPDrainRate;
+    int CircleSize;
+    float OverallDifficulty;
+    int ApproachRate;
+};
+
+struct TimingPoint_Set {
+    int time;
+    double beatLength;
+    int meter;
+    int Volume;
+    bool uninherited;
+    int effects;
+};
+
+extern General_Set M_General;
+extern MetaData_Set M_MetaData;
+extern Difficulty_Set M_Difficulty;
+extern std::vector<TimingPoint_Set> TimingPoints;
+extern std::vector<std::vector<int>> NotePoints;
+extern std::vector<std::vector<int>> ImagePoints;
+extern int TPoint_array_section;
+extern int Last_Note_pos;
+extern int _KEY_COUNT_;
+
+void note_info(std::string str);
+int Trim(std::string line, std::string line2);
+void ReadProperty_General(std::string str);
+void ReadProperty_MetaData(std::string str);
+void ReadProperty_TimingPoint(std::string str);
+void ReadProperty_Difficulty(std::string str);
+void ReadLine_Check(std::string str, int section);
+int LoadMapFile(std::string beatmap);
