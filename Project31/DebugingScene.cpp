8011655c95fa8b58de3636a1972d1e7ed70fa3c1
ElNath05 <121789470+ElNath05@users.noreply.github.com>
2024-01-02 02:23:31 +0900

    DebugingScene Edit

--- Project31/DebugingScene.cpp
+++ Project31/DebugingScene.cpp
@@ -1,8 +1,8 @@
 #include "DebugingScene.h"
 
-DebuggingScene::DebuggingScene(float width, float height) : setting_ui(150,175,500,250) {
-	this->width = width;
-	this->height = height;
+DebuggingScene::DebuggingScene(float width, float height) : setting_ui(150, 175, 500, 250) {
+    this->width = width;
+    this->height = height;
 }
 
 DebuggingScene::~DebuggingScene() {
@@ -11,12 +11,12 @@ DebuggingScene::~DebuggingScene() {
 
 
 void DebuggingScene::update(float dt) {
-	// 备泅何
+    // 备泅何
 }
 
 void DebuggingScene::onActivate() {
-	char keys[5] = "DFJK";
-	setting_ui.setAllkey(keys);
+    char keys[5] = "DFJK";
+    setting_ui.setAllkey(keys);
 }
 
 void DebuggingScene::onDeactivate() {
@@ -24,20 +24,23 @@ void DebuggingScene::onDeactivate() {
 }
 
 void DebuggingScene::draw(sf::RenderWindow& window) {
-	window.draw(setting_ui);
+    window.draw(setting_ui);
 }
 
 Signal DebuggingScene::handleInput(sf::Event event, sf::RenderWindow& window) {
-	if (event.type == sf::Event::KeyPressed)
-	{
-		if (event.key.code == sf::Keyboard::Escape)
-			return Signal::GoToMainMenu;
+    if (event.type == sf::Event::KeyPressed)
+    {
+        if (event.key.code == sf::Keyboard::Escape)
+            return Signal::GoToMainMenu;
 
-		if (event.key.code == sf::Keyboard::Left) {
-			
-		}else if (event.key.code == sf::Keyboard::Right) {
-
-		}
-	}
-	return Signal::None;
+        if (event.key.code == sf::Keyboard::Left) {
+            char keys[5] = "DFJK";
+            setting_ui.setAllkey(keys);
+        }
+        else if (event.key.code == sf::Keyboard::Right) {
+            char keys[5] = "AS;'";
+            setting_ui.setAllkey(keys);
+        }
+    }
+    return Signal::None;
 }
